bbbfa614b63c6ee52f32c88a071da27a6eb242aa
shubham mahore <shubhammahore@gmail.com>
2021-11-03 17:14:22 +0530

    Axios API call

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import logo from './logo.svg';
 import './App.css';
-
+import axios from "axios";
+import Message from "./todos";
 function App() {
+  const initialTutorialState = {
+    userId: 0,
+    id: 0,
+    title: "",
+    completed: false
+  };
+ 
+  
+
+  const [activities, setActivities] = useState<Message>(initialTutorialState);
+  
+  useEffect(() => {
+    axios
+      .get("https://localhost:44362/WeatherForecast")
+      .then((response) => {
+        console.log(response.data);
+        setActivities(response.data as Message);//Line where getting error Type is unknown
+      });
+      
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+         
+          id : {activities.id}
+         <br/>
+          userId : {activities.userId}
+          <br/>
+          title : {activities.title}
+          <br/>
+          completed : {activities.completed}
+          
+        
       </header>
     </div>
   );
---
+++ src/todos.ts
@@ -0,0 +1,8 @@
+export default interface Message {
+    userId: number;
+    id: number;
+    title: string;
+    completed: boolean;
+}
+
+  
